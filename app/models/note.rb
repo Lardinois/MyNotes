6aca42165b3219c3142084ea4d55925b502d35a9
Lardinois <jlardinois@lg.greisch.com>
2012-07-05 16:45:31 +0200

    bypass de style

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,7 @@ class Note < ActiveRecord::Base
 	def self.searchInTitle(string)
 		where(:title => string)
 	end
+
+	validates_presence_of :title, :user, :content
+	validates_format_of :user, :with => /\A[a-zA-Z]+\z/, :message => "Uniquement des lettres"
 end
